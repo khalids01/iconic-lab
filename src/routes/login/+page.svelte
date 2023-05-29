2126a3190241b6826d446733e5f57833d5663d67
khalids01 <khalids8774@gmail.com>
2023-05-29 23:44:14 +0600

    login page design done

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -1,37 +1,36 @@
 <script>
-	import Icon from '@iconify/svelte';
+	import Google from '../../components/Google.button.svelte';
+	import Github from '../../components/Github.button.svelte';
+	import Divider from '../../components/Divider.svelte';
 </script>
 
 <section class="container mx-auto px-4 pb-8 pt-16">
 	<h1 class="text-center mb-8 text-4xl">Login</h1>
-	<div class="wrapper mx-auto bg-slate-50/5 px-6 py-8 rounded-lg max-w-sm shadow-lg">
-        <form class="flex flex-col gap-4" method="POST">
-			<button class="btn bg-slate-900">
-				<Icon icon="skill-icons:github-light" class="text-xl flex gap-" />
-				<span> Github </span>
-			</button>
-			<button class="btn bg-slate-800">
-				<Icon icon="flat-color-icons:google" class="text-xl flex gap-" />
-				<span> Google </span>
-			</button>
+	<div class="wrapper mx-auto bg-slate-50/5 px-8 py-10 rounded-xl max-w-md shadow-xl">
+	<h2 class="text-center mb-8 text-2xl">Welcome back</h2>
+		<form class="flex flex-col gap-4" method="POST">
+			<Github type="button" onclick={() => console.log('github login')} />
+			<Google type="button" onclick={() => console.log('google login')} />
 		</form>
-        <div class="flex w-full justify-between relative py-6 px-1">
-            <hr class="relative z-0 w-full max-w-[150px] h-px" />
-            <span class='z-10 absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 text-slate-400'>or</span>
-            <hr class="relative z-0 w-full max-w-[150px] h-px" />
-        </div>
+		<Divider text="or" py='py-7'/>
 		<form action="?/login" method="POST">
 			<label class="label mb-4">
-				<input class="block !mb-4 input py-2" type="email" placeholder="mail@domain.com" />
-				<!-- <div class="input-group flex"> -->
+				<!-- <div class="input-group flex">
+					<input class=" w-[70%] input py-3" type="email" placeholder="mail@domain.com" />
 
-				<!-- <button type="submit" class="btn btn-sm variant-filled-primary w-auto "> Send Email </button> -->
-				<!-- </div> -->
-				<button type="submit" class="btn text-sm w-full variant-filled-primary w-auto">
-					Send Email
+					<button type="submit" class="btn btn-sm variant-filled-primary w-auto">
+						Send magic link
+					</button>
+				</div> -->
+				<input
+					class="block placeholder:text-center text-center !mb-4 input py-3"
+					type="email"
+					placeholder="mail@domain.com"
+				/>
+				<button type="submit" class="btn py-3 text-lg variant-filled-primary w-full">
+					Send magic link
 				</button>
 			</label>
 		</form>
-		
 	</div>
 </section>
