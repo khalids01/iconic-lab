bc46cb607cf3734b80fa6fb6232bdad836699308
khalids01 <khalids8774@gmail.com>
2023-05-30 01:33:15 +0600

    homepage design start

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,47 +1,48 @@
-<script lang='ts'>
-	import 'tailwindcss/tailwind.css'
+<script lang="ts">
+	import 'tailwindcss/tailwind.css';
 	import '@skeletonlabs/skeleton/themes/theme-skeleton.css';
 	import '@skeletonlabs/skeleton/styles/skeleton.css';
 	import '../app.postcss';
-	import { AppShell, AppBar } from '@skeletonlabs/skeleton';
 </script>
 
-<!-- App Shell -->
-<AppShell>
-	<svelte:fragment slot="header">
-		<!-- App Bar -->
-		<AppBar>
-			<svelte:fragment slot="lead">
-				<strong class="text-xl uppercase">Skeleton</strong>
-			</svelte:fragment>
-			<svelte:fragment slot="trail">
-				<a
-					class="btn btn-sm variant-ghost-surface"
-					href="https://discord.gg/EXqV7W8MtY"
-					target="_blank"
-					rel="noreferrer"
-				>
-					Discord
-				</a>
-				<a
-					class="btn btn-sm variant-ghost-surface"
-					href="https://twitter.com/SkeletonUI"
-					target="_blank"
-					rel="noreferrer"
-				>
-					Twitter
-				</a>
-				<a
-					class="btn btn-sm variant-ghost-surface"
-					href="https://github.com/skeletonlabs/skeleton"
-					target="_blank"
-					rel="noreferrer"
-				>
-					GitHub
-				</a>
-			</svelte:fragment>
-		</AppBar>
-	</svelte:fragment>
-	<!-- Page Route Content -->
-	<slot />
-</AppShell>
+<header class="">
+	<nav class="container max-w-7xl mx-auto w-full flex justify-between">
+		<a class="btn btn-sm variant-ghost-surface px-6" href="/login"> Login </a>
+	</nav>
+</header>
+
+<main class="relative h-full">
+	<div class="glowing-bg z-0 absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2" />
+	<div class="relative z-10 h-full">
+		<slot />
+	</div>
+</main>
+
+<style lang="postcss">
+	.glowing-bg {
+		@apply w-80 h-80;
+	}
+	.glowing-bg {
+		@apply absolute z-[-1] rounded-full blur-[150px] transition-all;
+		animation: pulse 5s cubic-bezier(0, 0, 0, 0.5) infinite, glow 15s linear infinite;
+	}
+	@keyframes glow {
+		0% {
+			@apply bg-primary-400/50;
+		}
+		33% {
+			@apply bg-secondary-400/50;
+		}
+		66% {
+			@apply bg-tertiary-400/50;
+		}
+		100% {
+			@apply bg-primary-400/50;
+		}
+	}
+	@keyframes pulse {
+		50% {
+			scale: 1.1;
+		}
+	}
+</style>
